bbb3a9b9e0ef3f961a99c50db022dd2f1c0c43fd
manishamuplam@gmail.com <manishamulam@gmail.com>
2022-06-29 22:17:58 +0530

    added:attendence

--- employeeWage.java
+++ employeeWage.java
@@ -1,10 +1,22 @@
 package com.bridge.labzs;
+import java.util.Random;
 
 public class employeeWage {
         public static void main(String[] args) {
             System.out.println("Welcome to Employee Wage Computation Program");
 
+            Random random =new Random();
+            int attendance=random.nextInt(2);
+
+            if(attendance == 1)
+                System.out.println("Employee is present");
+            else
+                System.out.println("Employee is absent");
+
+
+
         }
     }
+
 
 
