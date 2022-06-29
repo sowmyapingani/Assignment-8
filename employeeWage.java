51e0d9b08e4cd7edbea11d1cbddda400c0f7ae50
manishamuplam@gmail.com <manishamulam@gmail.com>
2022-06-29 23:07:02 +0530

    added:wages

--- employeeWage.java
+++ employeeWage.java
@@ -2,30 +2,30 @@ package com.bridge.labzs;
 import java.util.Random;
 
 public class employeeWage {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
-        Random random = new Random();
-        int attendance = random.nextInt(2);
-        int wagePerHour = 20;
-        int fullDayHours = 8;
-        int partTimeHours = 4;
-        int wagePerDay = 0;
+            public static void main(String[] args) {
+                System.out.println("Welcom to Employee Wage Camputation Program");
+                System.out.println("Welcom to Employee Wage Camputation Program");
+                Random random = new Random();
+                int wagePerHour = 20;
+                int fullDayHours = 8;
+                int partTimeHours = 4;
+                int monthlySalary = 0;
+                int wagePerDay = 0;
 
-        switch (attendance) {
-            case 0:
-                System.out.println("Employee Absent");
-                break;
-            case 1:
-                wagePerDay = partTimeHours * wagePerHour;
-                break;
-            default:
-                wagePerDay = wagePerHour * fullDayHours;
+                for (int day = 1; day <= 20; day++) {
+                    int attendance = random.nextInt(3);
+                    wagePerDay = 0;
+                    switch (attendance) {
+                        case 0:
+                            System.out.println("Employee Absent");
+                            break;
+                        case 1:
+                            wagePerDay = partTimeHours * wagePerHour;
+                            break;
+                        default:
+                            wagePerDay = wagePerHour * fullDayHours;
+                    }
+                    monthlySalary = monthlySalary + wagePerDay;
+                }
+            }
         }
-        if (attendance == 1) {
-            System.out.println("Employee Present");
-            wagePerDay = wagePerHour * fullDayHours;
-        } else
-            System.out.println("Employee Absent");
-        System.out.println("Employee Wage Per Day:" + wagePerDay);
-    }
-}
