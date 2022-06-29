d97992ebf38e57c6e3934eccb61a022c7901ebac
manishamuplam@gmail.com <manishamulam@gmail.com>
2022-06-29 22:48:55 +0530

    added:message

--- employeeWage.java
+++ employeeWage.java
@@ -2,21 +2,21 @@ package com.bridge.labzs;
 import java.util.Random;
 
 public class employeeWage {
-        public static void main(String[] args) {
-            System.out.println("Welcome to Employee Wage Computation Program");
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+        Random random = new Random();
+        int attendance = random.nextInt(2);
 
-            Random random =new Random();
-            int attendance=random.nextInt(2);
+        int wagePerHour = 20;
+        int fullDayHours = 8;
+        int wagePerDay = wagePerHour + fullDayHours;
 
-            if(attendance == 1)
-                System.out.println("Employee is present");
-            else
-                System.out.println("Employee is absent");
-
-
-
+        if (attendance == 1) {
+            System.out.println("Employee is present");
+            wagePerDay = wagePerHour * fullDayHours;
+        } else {
+            System.out.println("Employee is absent");
+            System.out.println("Employee wage Per Day : " + wagePerDay);
         }
     }
-
-
-
+}
