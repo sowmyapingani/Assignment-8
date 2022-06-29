b448d9550d034d36e64ae1888314d4632027ccc0
manishamuplam@gmail.com <manishamulam@gmail.com>
2022-06-29 23:02:43 +0530

    added:switchcase

--- employeeWage.java
+++ employeeWage.java
@@ -3,13 +3,24 @@ import java.util.Random;
 
 public class employeeWage {
     public static void main(String[] args) {
-        System.out.println("Welcom to Employee Wage Camputation Program");
+        System.out.println("Welcome to Employee Wage Computation Program");
         Random random = new Random();
         int attendance = random.nextInt(2);
         int wagePerHour = 20;
         int fullDayHours = 8;
-        int wagePerDay = 8;
+        int partTimeHours = 4;
+        int wagePerDay = 0;
 
+        switch (attendance) {
+            case 0:
+                System.out.println("Employee Absent");
+                break;
+            case 1:
+                wagePerDay = partTimeHours * wagePerHour;
+                break;
+            default:
+                wagePerDay = wagePerHour * fullDayHours;
+        }
         if (attendance == 1) {
             System.out.println("Employee Present");
             wagePerDay = wagePerHour * fullDayHours;
@@ -17,4 +28,4 @@ public class employeeWage {
             System.out.println("Employee Absent");
         System.out.println("Employee Wage Per Day:" + wagePerDay);
     }
-    }    
+}
